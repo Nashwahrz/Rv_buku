087899b93d7180d218058f3a1afed361bcfedfa1
NASHWA HARZATHI RAHMA <162123885+Nashwahrz@users.noreply.github.com>
2024-09-09 20:27:53 +0700

    Recycle View only

---
+++ app/src/main/java/com/nashwa/rv_buku/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.nashwa.rv_buku
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nashwa.rv_buku.Model.ModelBuku
+import com.nashwa.rv_buku.adapter.BukuAdapter
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  rv_buku : RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        rv_buku = findViewById(R.id.rv_buku)
+
+        //ini kita bikin array data
+        val listBuku = listOf(
+            ModelBuku(title = "Buku Android Kotlin 2024", penulis = "Rizki Syaputra"),
+            ModelBuku(title = "Buku Android Kotlin2 2024", penulis = "Rizki Syaputra"),
+            ModelBuku(title = "Buku Android Kotlin3 2024", penulis = "Rizki Syaputra"),
+            ModelBuku(title = "Buku Android Kotlin4 2024", penulis = "Rizki Syaputra"),
+            ModelBuku(title = "Buku Android Kotlin5 2024", penulis = "Rizki Syaputra"),
+        )
+
+        val nBukuAdapter = BukuAdapter(listBuku)
+        rv_buku.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = nBukuAdapter
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/nashwa/rv_buku/Model/ModelBuku.kt
@@ -0,0 +1,6 @@
+package com.nashwa.rv_buku.Model
+
+data class ModelBuku (
+    val title: String,
+    val penulis: String
+)
